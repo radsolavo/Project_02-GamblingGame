96d0fac28b144e0cd1e1ed7180e1e439856f181d
Radek Śnieżek <radeksniezek96@gmail.com>
2022-11-05 17:05:19 +0100

    Initial commit

---
+++ Wallet.js
@@ -0,0 +1,32 @@
+class Wallet {
+    constructor(money) {
+let _money = money;
+
+//Pobieranie aktualnej zawartości portfela
+this.getWalletValue = () => _money;
+    
+
+    // Sprawdzanie czy użytkownik ma odpowiednią ilość środków
+
+    this.checkCanPlay=(value) => {
+        if (_money>=value) return true;
+        return false
+    }
+    this.changeWallet = (value, type ="+")=>{
+if(typeof value === "number" && !isNaN(value))
+{
+if (type === "+"){
+    return _money += value;
+} else if (type === "-"){
+    return _money -=value
+} else {
+    throw new Error ("nieprawidłowy typ działania")
+}
+} else {
+    console.log(typeof value);
+    throw new Error ("nieprawidłowa liczba")
+}
+    }
+}
+
+}
